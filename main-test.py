426910bd0bebd54e91cab4ece5b33d72ceacedb2
staylor <staylor@evidation.com>
2022-01-12 15:27:29 -0800

    Added more

---
+++ main-test.py
@@ -0,0 +1,9 @@
+from main import Add
+
+def TestAdd():
+    assert(Add(5,6) == 11)
+
+
+if __name__ == "__main__":
+    TestAdd()
+    print("Successfully tested main()")
---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+def Add(x, y):
+    return x + y
+
+
+def SayHello(someone):
+    print("Hello {}!".format(someone))
+
+if __name__ == "__main__":
+    SayHello("Stanley")
